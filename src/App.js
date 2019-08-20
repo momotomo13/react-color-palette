00c9e8b22db390aefaeb7fca2038e50059e0ef58
momotomo <tomoya@TomoyanoMacBook-Pro.local>
2019-08-20 11:11:30 +0900

    Inplemented basic react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[3])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>PALETTE</h1>} />
+      <Route exact path="/palette/:id" render={() => <h1>PALETTE item</h1>} />
+    </Switch>
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[3])} />
+    // </div>
   );
 }
 
