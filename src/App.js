2a603e1c06fff2ef4de89ccb372343bc53a4eb48
momotomo <tomoya@TomoyanoMacBook-Pro.local>
2019-08-20 11:26:37 +0900

    fix palette route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>PALETTE</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>PALETTE item</h1>} />
-    </Switch>
-    // <div>
-    //   <Palette palette={generatePalette(seedColors[3])} />
-    // </div>
-  );
-}
+export default class App extends Component {
+  //seedColorからidがmatchするパレットを返す
+  findPalette(id) {
+    return seedColors.find(palette => {
+      return palette.id === id;
+    });
+  }
 
-export default App;
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            //findPaletteでidにマッチするパレットを変換後generatePaletteでshade付きのパレットを生成し、Paletteコンポーネントに渡す
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[3])} />
+      // </div>
+    );
+  }
+}
