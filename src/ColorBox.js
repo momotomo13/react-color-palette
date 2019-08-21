7274b132e4dc69b171bf45273bc57559a4d0a843
momotomo <tomoya@TomoyanoMacBook-Pro.local>
2019-08-21 13:46:23 +0900

    added link to more

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,49 +1,52 @@
-import React, { Component } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom";
 import "./ColorBox.css";
 
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            copied: false
-        };
-        this.changeCopyState = this.changeCopyState.bind(this);
-    }
+  changeCopyState() {
+    // copyされたら1500msで表示、非表示の切り替え
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
 
-    changeCopyState(){
-        // copyされたら1500msで表示、非表示の切り替え
-        this.setState({copied: true}, () => {
-            setTimeout(() => this.setState({ copied:false }), 1500);
-        });
-    }
-
-    render() {
-        // this.propsを省略
-        const {name, color} = this.props;
-        const {copied} = this.state;
-        return (
-            // textに設定されてるものをclipboardに設定
-            <CopyToClipboard text={color} onCopy={this.changeCopyState}>
-                <div style={{ background: color }} className="ColorBox">
-                    <div 
-                        style={{ background: color }} 
-                        className={`copy-overlay ${copied && 'show'}`} 
-                    />
-                    <div className={`copy-msg ${copied && 'show'}`}>
-                        <h1>Copied</h1>
-                        <p>{color}</p>
-                    </div>
-                    <div className="copy-container">
-                        <div className="box-content">
-                            <span>{ name }</span>
-                        </div>
-                        <button className="copy-button">Copy</button>
-                    </div>
-                    <span className="see-more">MORE</span>
-                </div>
-            </CopyToClipboard>
-        )
-    }
+  render() {
+    // this.propsを省略
+    const { name, color } = this.props;
+    const { copied } = this.state;
+    return (
+      // textに設定されてるものをclipboardに設定
+      <CopyToClipboard text={color} onCopy={this.changeCopyState}>
+        <div style={{ background: color }} className="ColorBox">
+          <div
+            style={{ background: color }}
+            className={`copy-overlay ${copied && "show"}`}
+          />
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1>Copied</h1>
+            <p>{color}</p>
+          </div>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
+          </div>
+          {/* transitionをつけた際にcopyアニメーションをstopするためにstopPropagation追加 */}
+          <Link to="/" onClick={e => e.stopPropagation()}>
+            <span className="see-more">MORE</span>
+          </Link>
+        </div>
+      </CopyToClipboard>
+    );
+  }
 }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -34,7 +34,7 @@ const styles = {
 };
 
 class PaletteList extends Component {
-  //historyを使い個別のパレットページへ
+  //historyを使い個別のpaletteへ
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
