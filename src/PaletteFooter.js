1334a32eec48e48269c842a4d090b4fcfa9d65e8
momotomo <tomoya@TomoyanoMacBook-Pro.local>
2019-08-22 18:01:13 +0900

    create styles folder and move styles to it

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,13 +1,15 @@
 import React from "react";
+import styles from "./styles/PaletteFooterStyle";
+import { withStyles } from "@material-ui/styles";
 
 const PaletteFooter = props => {
-  const { paletteName, emoji } = props;
+  const { paletteName, emoji, classes } = props;
   return (
-    <footer className="palette-footer">
+    <footer className={classes.PaletteFooter}>
       {paletteName}
-      <span className="emoji">{emoji}</span>
+      <span className={classes.emoji}>{emoji}</span>
     </footer>
   );
 };
 
-export default PaletteFooter;
+export default withStyles(styles)(PaletteFooter);
---
+++ src/styles/ColorBoxStyles.js
@@ -0,0 +1,114 @@
+import chroma from "chroma-js";
+
+export default {
+  ColorBox: {
+    width: "20%",
+    //single color paletteの高さをダイナミックに振り分け
+    height: props => (props.showingFullPalette ? "25%" : "50%"),
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    marginBottom: "-3.5px",
+    "&:hover button": {
+      opacity: 1
+    }
+  },
+  //明るさに応じて文字色を変える
+  copyText: {
+    color: props => (chroma(props.color).luminance() >= 0.7 ? "black" : "white")
+  },
+  copyName: {
+    color: props =>
+      chroma(props.color).luminance() >= 0.08 ? "white" : "black"
+  },
+  seeMore: {
+    color: props =>
+      chroma(props.color).luminance() >= 0.8 ? "black" : "white",
+    background: "rgba(255, 255, 255, .3)",
+    position: "absolute",
+    border: "none",
+    right: "0px",
+    bottom: "0px",
+    width: "50px",
+    height: "20px",
+    fontSize: ".7rem",
+    textAlign: "center",
+    lineHeight: "20px",
+    textTransform: "uppercase"
+  },
+  copyButton: {
+    color: props =>
+      chroma(props.color).luminance() >= 0.7 ? "black" : "white",
+    width: "100px",
+    height: "30px",
+    position: "absolute",
+    display: "inline-block",
+    top: "50%",
+    left: "50%",
+    marginLeft: "-50px",
+    marginTop: "-15px",
+    textAlign: "center",
+    outline: "none",
+    background: "rgba(255, 255, 255, .3)",
+    fontSize: ".9rem",
+    lineHeight: "30px",
+    textTransform: "uppercase",
+    border: "none",
+    borderRadius: "10px",
+    opacity: 0
+  },
+  boxContent: {
+    position: "absolute",
+    left: "5px",
+    bottom: "5px",
+    color: "black",
+    letterSpacing: "1.4px",
+    fontSize: ".7rem"
+  },
+  copyOverlay: {
+    opacity: 0,
+    zIndex: 0,
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out"
+  },
+  showOverlay: {
+    opacity: 1,
+    transform: "scale(50)",
+    zIndex: 10,
+    position: "absolute"
+  },
+  copyMsg: {
+    position: "fixed",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "2.6rem",
+    transform: "scale(0.1)",
+    opacity: 0,
+    color: "white",
+    "& h1": {
+      fontWeight: "bold",
+      textShadow: "2px 2px 40px rgba(0, 0, 0, .1)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: 0,
+      padding: "1rem"
+    },
+    "& p": {
+      fontSize: ".9rem"
+    }
+  },
+  showMessege: {
+    opacity: 1,
+    transform: "scale(1)",
+    zIndex: 20,
+    transition: "all 0.2s ease-in-out",
+    transitionDelay: "0.3s"
+  }
+};
