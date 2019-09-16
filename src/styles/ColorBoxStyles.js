58fe96af58daa6fc3c2cc659a40d5c0b43d71854
momotomo <tomoya@TomoyanoMacBook-Pro.local>
2019-09-16 15:37:23 +0900

    added responsive helper and styled colorBox

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from "chroma-js";
+import sizes from "./sizes";
 
 export default {
   ColorBox: {
@@ -11,6 +12,19 @@ export default {
     marginBottom: "-3.5px",
     "&:hover button": {
       opacity: 1
+    },
+    //media query
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: props => (props.showingFullPalette ? "20%" : "40%")
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: props => (props.showingFullPalette ? "10%" : "40%")
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: props => (props.showingFullPalette ? "5%" : "10%")
     }
   },
   //明るさに応じて文字色を変える
